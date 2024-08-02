622f44024cbce21685966183661c3ca71c566b65
Dr.Guo <guoxiaozhong1990@outlook.com>
2024-08-02 11:14:58 +0800

    Update package name and description in Cargo.toml.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,7 @@ use tauri_plugin_autostart::{MacosLauncher, ManagerExt};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        // .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_stronghold::Builder::new(|_pass| todo!()).build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
