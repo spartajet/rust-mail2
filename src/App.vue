efd97ae92a11fcdb6b7cd69ecd6fd641edf420b9
Dr.Guo <guoxiaozhong1990@outlook.com>
2024-10-16 20:20:20 +0800

    大布局

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
+  <v-layout class="rounded rounded-md">
+    <v-system-bar color="grey-darken-3"></v-system-bar>
+    <v-navigation-drawer
+        color="grey-darken-2"
+        width="150"
+        permanent
+    ></v-navigation-drawer>
+
+    <v-navigation-drawer
+        color="grey-darken-1"
+        width="150"
+        permanent
+    ></v-navigation-drawer>
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <HelloWorld></HelloWorld>
     </v-main>
-  </v-app>
+  </v-layout>
 </template>
 
 <script setup lang="ts">
-  //
+//
+import HelloWorld from "@/components/HelloWorld.vue";
 </script>
